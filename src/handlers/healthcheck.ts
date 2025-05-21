66b59e9260dffd70212d18f325c869f8198528df
racer242 <racer242@gmail.com>
2025-05-21 12:52:51 +0300

    First commit

---
+++ src/handlers/healthcheck.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import logger from '../utils/logger';
+
+export const healthCheck = async (_req: express.Request, res: express.Response) => {
+  logger.info('Server is starting');
+  res.send('Server is working');
+};
